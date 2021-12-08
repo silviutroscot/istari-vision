e49fe6a93ceb773cb2c92830259597a1e789edfd
Silviu Troscot <silviu@gstechnologies.io>
2021-12-07 20:58:03 -0800

    added go mod

---
+++ pkg/webservice/webservice.go
@@ -0,0 +1,42 @@
+package webservice
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	gin.SetMode(gin.DebugMode)
+}
+
+type API struct {
+	Address string
+
+	engine  *gin.Engine
+	service *service.Service
+}
+
+// NewAPI creates a new instance of a WebServer, which encapsulates the router and the dependencies of the WebService
+func NewAPI(service *service.Service) *API {
+	// create a new HTTP router engine
+	engine := gin.New()
+	engine.Use(gin.Recovery())
+
+	engine.GET("/health.txt", func(c *gin.Context) {
+		c.String(http.StatusOK, "ok")
+	})
+
+	return &API{
+		engine:  engine,
+		service: service,
+	}
+}
